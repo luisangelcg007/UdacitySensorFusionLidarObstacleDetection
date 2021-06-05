24645f6de337660d848b6cefaed804d61eab4b1d
luisangelcg007 <luisangelcg007@gmiail.com>
2021-06-05 06:02:09 +0000

    3D RANSAC algorithm implementation

--- SFND_Lidar_Obstacle_Detection/src/quiz/ransac/ransac2d.cpp
+++ SFND_Lidar_Obstacle_Detection/src/quiz/ransac/ransac2d.cpp
@@ -63,14 +63,98 @@ pcl::visualization::PCLVisualizer::Ptr initScene()
 
 std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
 {
-	std::unordered_set<int> inliersResult;
-	srand(time(NULL));
+	//std::unordered_set<int> inliersResult;
+	//srand(time(NULL));
 	
 	// TODO: Fill in this function
 
 	// For max iterations 
 
 	// Randomly sample subset and fit line
+
+	// Measure distance between every point and fitted line
+	// If distance is smaller than threshold count it as inlier
+
+	// Return indicies of inliers from fitted line with most inliers
+	
+	//return inliersResult;
+
+	auto startTime = std::chrono::steady_clock::now();
+
+	std::cout << "Starting Ransac\n";
+	std::unordered_set<int> inliersResult;
+	srand(time(NULL));
+	
+	
+	for(int i = 0; i < maxIterations; i++) {
+		std::cout << "START: Iteration " << i << "\n";
+		
+		std::unordered_set<int> inliers;
+		
+		while (inliers.size() < 3) {
+			int random_point = (static_cast<double>(std::rand()) / RAND_MAX) * cloud->points.size();
+			inliers.insert(random_point);
+		}
+
+		auto iterator = inliers.begin();
+		
+		pcl::PointXYZ p1 = cloud->points[*iterator];
+		float x1 = p1.x;
+		float y1 = p1.y;
+		float z1 = p1.z;
+		++iterator;
+
+		pcl::PointXYZ p2 = cloud->points[*iterator];
+		float x2 = p2.x;
+		float y2 = p2.y;
+		float z2 = p2.z;
+		++iterator;
+		
+		pcl::PointXYZ p3 = cloud->points[*iterator];
+		float x3 = p3.x;
+		float y3 = p3.y;
+		float z3 = p3.z;
+
+		std::tuple<float, float, float> v1(x2 - x1, y2 - y1, z2 - z1);
+		std::tuple<float, float, float> v2(x3 - x1, y3 - y1, z3 - z1);
+
+		std::tuple<float, float, float> normal((y2 - y1) * (z3 - z1) - (z2 - z1) * (y3 - y1),
+											   (z2 - z1) * (x3 - x1) - (x2 - x1) * (z3 - z1),
+											   (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1));
+
+		float A = std::get<0>(normal);
+		float B = std::get<1>(normal);
+		float C = std::get<2>(normal);
+		float D = -(A * x1 + B * y1 + C * z1);
+
+		// std::cout << "The line's equation is: " << A << "x" << " + " << B << "y" << " + " << C << "\n";
+		
+		for(int i = 0; i < cloud->points.size(); i++)  {
+			pcl::PointXYZ p0 = cloud->points[i];
+
+			if (inliers.count(i) > 0)
+				continue;
+
+			float d = std::fabs(A * p0.x + B * p0.y + C * p0.z + D) / std::sqrt(A * A + B * B + C * C);
+			// std::cout << "Point " << "(" << p0.x << "," << p0.y << "), " << "distance to line: " << d << "\n";
+			
+			if (d <= distanceTol) {
+				inliers.insert(i);
+			}
+		}
+		if (inliers.size() > inliersResult.size()) {
+			inliersResult = inliers;
+		}
+		std::cout << "END: Iteration " << i << ", with " << inliers.size() << " inlier points.\n\n";
+	}
+
+	auto endTime = std::chrono::steady_clock::now();
+	auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+    std::cout << "RANSAC for planes took " << elapsedTime.count() << " milliseconds" << std::endl;
+
+
+	
+	// Randomly sample a two-point-subset and fit line
 
 	// Measure distance between every point and fitted line
 	// If distance is smaller than threshold count it as inlier
@@ -88,11 +172,11 @@ int main ()
 	pcl::visualization::PCLVisualizer::Ptr viewer = initScene();
 
 	// Create data
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData();
-	
+	//pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData();
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData3D();
 
 	// TODO: Change the max iteration and distance tolerance arguments for Ransac function
-	std::unordered_set<int> inliers = Ransac(cloud, 0, 0);
+	std::unordered_set<int> inliers = Ransac(cloud, 50, 0.3);
 
 	pcl::PointCloud<pcl::PointXYZ>::Ptr  cloudInliers(new pcl::PointCloud<pcl::PointXYZ>());
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudOutliers(new pcl::PointCloud<pcl::PointXYZ>());
